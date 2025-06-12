eaedbd1e47f2923f9e0a2f9016a1f95f415feb75
Guilherme <gui.carmonunes@gmail.com>
2025-06-12 17:33:13 +0100

    Teste basico sintax tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
 import './App.css';
 
-let name: string = 'Guilherme';
-let age: number = 30;
+// let name: string = 'Guilherme';
+// let age: number = 30;
+// let isDeveloper: boolean;
+// isDeveloper = true;
+// let role: [string, number, boolean];
+// role = ['Guilherme', 30, true];
+// 
+// type Dev = {
+//   neme: String;
+//   age: number | string; 
+//   isDeveloper?: boolean;  
+// };  
+// 
+// let dev: Dev = {
+//   neme: 'Guilherme',
+//   age: 30,
+// };
+// 
+// let lotsOfDevs: Dev[];
+
+
 
 
 function App() {
