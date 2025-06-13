af0d0271eb91f65187be9eecc73dbd39da27ac83
Guilherme <gui.carmonunes@gmail.com>
2025-06-13 10:21:50 +0100

    Adição de CSS basico, fonte ...)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,28 @@
 import React from 'react';
 import './App.css';
 
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <span className='heading'>Taskforce</span>
+    
+    </div>
+  );
+};
+
+// Outra forma de representar a mesma função:
+// function App() {
+// return (
+//     <div className="App">      
+//       Hello World!!
+//     </div>
+//   );
+// }
+
+export default App;
+
+// -----------------------------------------------------
 // let name: string = 'Guilherme';
 // let age: number = 30;
 // let isDeveloper: boolean;
@@ -21,15 +43,16 @@ import './App.css';
 // 
 // let lotsOfDevs: Dev[];
 
+// let printName: (name: string) => never;
+// 
+// type X = {
+//   a: string;
+//   b: number;
+// };
+// 
+// type Y = & {
+//   c: string;
+//   d: number;
+// };
 
-
-
-function App() {
-  return (
-    <div className="App">
-      Hello World!!
-    </div>
-  );
-}
-
-export default App;
+// ---------------------------------------------------------------
